f4775ee91907258fe3e0514f06e9bf6c05c50920
harshkaushik-cyber <kaushikharsh186@gmail.com>
2025-03-27 16:42:40 +0530

    fixg

--- src/component/serviceoffered/IosAppDevelopment/About.jsx
+++ src/component/serviceoffered/IosAppDevelopment/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import "./About.css"
+import "./about.css"
 import Aboutlogo from "./images/Ios-about.jpg"
 import AnimatedContent from "../../SingleComponents/AnimatedContent"
 
