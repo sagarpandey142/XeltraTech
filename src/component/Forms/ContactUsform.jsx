7252ff307b189e837c6082d82994a56acdf6ce6d
sagarpandey141 <121431268+sagarpandey141@users.noreply.github.com>
2025-03-27 14:14:56 +0530

    updatted code

--- src/component/Forms/ContactUsform.jsx
+++ src/component/Forms/ContactUsform.jsx
@@ -20,7 +20,7 @@ const ContactForm = () => {
     const toastId = toast.loading("Sending message...");
 
     try {
-        const response = await fetch('http://localhost:5000/contact', { 
+        const response = await fetch('https://xeltratech.onrender.com/contact', { 
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(formData),
